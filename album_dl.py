f3fb375f26506ad03e5eee09c129de08a01f6af6
Hasan Zahid <hasanthedev@gmail.com>
2018-06-22 22:17:27 -0500

    turning files into mp3

--- album_dl.py
+++ album_dl.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 from spotipy.oauth2 import SpotifyClientCredentials
-import spotify_config, spotipy, os, requests, bs4, youtube_dl
+import spotify_config, spotipy, os, requests, bs4, youtube_dl, re
 
 print("Enter the artist name")
 artist = input()
@@ -24,8 +24,6 @@ try:
 except WindowsError:
   print('dir already exists')
   dir = r'C:\Users\wazih\Desktop\songs\%s' %(album_name)
-
-print('directory created')
 
 client_credentials_manager = SpotifyClientCredentials(
     spotify_config.spotify_client_ID, spotify_config.spotify_client_SECRET)
@@ -54,6 +52,8 @@ for album in albums['items']:
     # list comprehens to generate an array with the track names
     track_names = [track['name'] for track in tracks['items']]
 
+print('tracks:', track_names)
+
 print('getting links from YT')
 links = []
 for track in track_names:
@@ -61,8 +61,8 @@ for track in track_names:
   res = requests.get('https://www.youtube.com/results?search_query=' + res_artist + track)
   soup = bs4.BeautifulSoup(res.text, "html.parser")
   # selecting the specific tag to get the link
-  ol = soup.select('ol .item-section a')
-  link = 'https://www.youtube.com'+ol[3].get('href')  
+  vid_id = soup.select('h3 > a')
+  link = 'https://www.youtube.com'+vid_id[0].get('href')  
   links.append(link)
 
 print(links)
@@ -72,3 +72,12 @@ os.chdir(dir)
 ydl_opts = {'format': 'bestaudio/best'}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
   ydl.download(links)
+
+# regex to get the song name before the extension
+name_regex =  re.compile(r'^.*(?=(\.))')
+# turning files into mp3
+for file in os.listdir(dir):
+  mo = name_regex.search(file)
+  os.rename(dir + '\\' + file, dir + '\\' + mo.group() + '.mp3')
+
+print('turned files into mp3')
