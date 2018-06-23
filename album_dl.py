098f98f60a3d7c509d13767ad7b25101a8e25a3e
Hasan Zahid <hasanthedev@gmail.com>
2018-06-22 20:51:37 -0500

    getting artist albums from artist spotify id

--- album_dl.py
+++ album_dl.py
@@ -9,3 +9,18 @@ album = input()
 
 client_credentials_manager = SpotifyClientCredentials(spotify_config.spotify_client_ID, spotify_config.spotify_client_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_artist_id(name):
+  results = sp.search(q='artist:' + name, type='artist')
+  items = results['artists']['items']
+  if len(items) > 0:
+      return items[0]['uri']
+  else:
+      return None
+
+artist_id = get_artist_id(artist)
+
+# getting albums from spotify for atist with artist id
+albums = sp.artist_albums(artist_id, limit=30)
+
+print(albums)
