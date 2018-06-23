b1cd97a96eab29d0cd0ca764b31a4220ff191b22
Hasan Zahid <hasanthedev@gmail.com>
2018-06-22 21:34:04 -0500

    getting video url from YT to feed into downloader

--- album_dl.py
+++ album_dl.py
@@ -1,8 +1,15 @@
 from spotipy.oauth2 import SpotifyClientCredentials
-import spotify_config, spotipy, os
+import spotify_config, spotipy, os, requests, bs4
 
 print("Enter the artist name")
 artist = input()
+
+name_split = artist.split(' ')
+res_artist = ''
+if len(name_split) > 1:
+  res_artist = '+'.join(name_split)
+else:
+  res_artist = artist
 
 print("Enter the album name you want to dl")
 album_name = input()
@@ -13,6 +20,7 @@ try:
   dir = r'C:\Users\wazih\Desktop\songs\%s' %(album_name)
   os.makedirs(dir)
 except WindowsError:
+  print('dir already exists')
   dir = r'C:\Users\wazih\Desktop\songs\%s' %(album_name)
 
 print('directory created')
@@ -44,4 +52,15 @@ for album in albums['items']:
     # list comprehens to generate an array with the track names
     track_names = [track['name'] for track in tracks['items']]
 
-print(track_names)
+print('getting links from YT')
+links = []
+for track in track_names:
+  # getting the url from youtube
+  res = requests.get('https://www.youtube.com/results?search_query=' + res_artist + track)
+  soup = bs4.BeautifulSoup(res.text, "html.parser")
+  # selecting the specific tag to get the link
+  ol = soup.select('ol .item-section a')
+  link = 'youtube.com'+ol[3].get('href')  
+  links.append(link)
+
+print(links)
